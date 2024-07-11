bc7617f766f24d1c1d160af911199e6b1fe3a41c
JaiPutti <140320230+JaiPutti@users.noreply.github.com>
2024-07-11 12:14:00 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,75 +1,59 @@
-function preload(){
+function preload() {
 
 }
 
-function setup()
-{
+function setup() {
 
-canvas = createCanvas(640, 480);
-canvas.position(110, 250);
-video = createCapture(VIDEO);
-video.hide();
-
-tint_color = "";
-
-image(img, 100, 280, 340, 0,)  
-
-fill(0, 128, 0);
-stroke(0, 128, 0);
-circle(350, 50, 70);
-
-image(img, 200, 280, 340, 0,)  
-
-fill(0, 128, 0);
-stroke(0, 128, 0);
-circle(350, 50, 70);
-
-image(img, 100, 0, 340, 0,)  
-
-fill(0, 128, 0);
-stroke(0, 128, 0);
-circle(350, 50, 70);
-
-image(img, 100, 0, 340, 0,)  
-
-fill(0, 128, 0);
-stroke(0, 128, 0);
-circle(350, 50, 70);
-
-createCanvas(100, 100);
-
-
-line(30, 20, 85, 75);
-
-createCanvas(100, 100);
-
-
-line(30, 30, 95, 75);
-
-}
-
-function draw()
-{
-
-    image(video, 0, 0, 340, 280);
-    tint(tint_color);
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO);
+    video.hide();
 
 
 
- 
-}
-
-function take_snapshot()
-{
-
-save ('student+name.png');
 
 
 }
 
-function filter_tint()
-{
+function draw() {
 
-tint_color = document.getElementById("color_name").value;
+    image(video, 230, 150, 220, 200);
+  
+
+   
+
+    fill(0, 230, 0);
+    stroke(0, 230, 0);
+    circle(50, 50, 80);
+
+
+    circle(50,430,80);
+    circle(590,430,80);
+    circle(590,50,80);
+
+
+
+    fill(450, 128, 0);
+    stroke(450, 128, 0);
+    
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(580, 90, 20, 300);
+
+
+
+}
+
+function take_snapshot() {
+
+    save('student+name.png');
+
+
+}
+
+function filter_tint() {
+
+    tint_color = document.getElementById("color_name").value;
 
 }
